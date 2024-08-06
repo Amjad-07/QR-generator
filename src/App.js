dde739cd0b2a897e7ae0c50f882b33b4d3610e49
Amjad-07 <mohamedamjad295@gmail.com>
2024-08-06 14:12:58 +0530

    created qr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './index.css';
 
-function App() {
+
+export const App = () => {
+  const [img,setimg]=useState("")
+  
+  const [load,success]=useState(false)
+  const [set,put]=useState("www.youtube.com")
+  const [size,setSize] = useState("150")
+  async function generateQr(){
+    success(true)
+    try{
+      const url= `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${encodeURIComponent(set)}`
+      setimg(url)
+
+    }catch(error){
+      console.log("Error Generating QR code",error);
+      
+
+    }finally{
+      success(false)
+
+    }
+    console.log(img);
+    
+
+
+  }
+  function downLoad(){
+    fetch(img).then((response)=>response.blob()).then((blob)=>{
+      const link = document.createElement("a")
+      link.href=URL.createObjectURL(blob)
+      link.download="QR.png"
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <div className="qr-container">
+      <h2>QR-generator</h2>
+      {load && <p>Please Wait...</p>}
+      {img && <img src={img} alt="" />}
 
-export default App;
+      <div className='qr-input'>
+      <label htmlFor="code">label for QR-code</label>
+      <input type="text" htmlFor="code"     placeholder='Enter data for QR-code' value={set}    onChange={(e) => put(e.target.value)}/><br />
+      <label htmlFor="down">Image-size(eg.150)</label>
+      <input type="text" htmlFor="down"placeholder='Enter Image-size'  value={size}
+          onChange={(event) => setSize(event.target.value)}/><br />
+      </div>
+      <button className='gene' disabled={load } onClick={generateQr}>Generate QR</button><br />
+      <button className='dwn' onClick={downLoad}>Download QR</button><br />
+    </div>
+
+    </>
+  )
+}
